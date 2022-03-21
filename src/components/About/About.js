cc9af7fe4de7a1155ccf5a375f3f62f2c22cd5e5
Witold Mikołajczak <witekmikolajczak@gmail.com>
2022-03-21 14:17:14 +0100

    commit before add weather-app to portfolio

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,9 +1,22 @@
 import Main from "../UI/Main/Main";
 import React from "react";
+
+import classes from "./About.module.scss";
+import image from "../../assets/WM_oryg.JPG";
 const About = () => {
   return (
     <div id="about-section">
-      <Main></Main>
+      <Main className={classes.about}>
+        <div className={classes.description}>
+          <p>
+            lorem lorem lorem lorem lorem loremlorem lorem lorem lorem lorem
+            lorem loremlorem lorem lorem lorem lorem lorem loremlorem lorem
+            lorem lorem lorem lorem loremlorem lorem lorem lorem lorem lorem
+            loremlorem lorem lorem lorem lorem lorem loremlorem
+          </p>
+        </div>
+        <div className={classes.image}>{/* <img src={image} /> */}</div>
+      </Main>
     </div>
   );
 };
